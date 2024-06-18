64863503bffb1eab64c2b3a9a60400f05a4c4e79
MA Developer <moisesna.deus@gmail.com>
2024-06-18 16:03:50 -0300

    Exercício com uso de ArrayList e Interface Comparable e uso da classe Collections.

---
+++ src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    double numero;
+    double numeroDobro;
+
+    void retornaDobro() {
+        numeroDobro = numero * 2;
+        System.out.println(numeroDobro);
+    }
+}
